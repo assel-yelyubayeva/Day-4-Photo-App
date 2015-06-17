3009caf31adee0ff5fbfe37889b674bb81e7be50
assel-yelyubayeva <assel.yelyubayeva@gmail.com>
2015-06-17 10:12:36 +0600

    Initial Commit

---
+++ PhotoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoApp
+//
+//  Created by Kuralai on 17.06.15.
+//  Copyright (c) 2015 Assel Yelyubayeva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
